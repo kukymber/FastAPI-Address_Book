97786b02b471ecbe323ecea29f88ae439f4a5f9a
anatolii <ostap_to@mail.ru>
2022-08-22 13:08:38 +0800

    do schemas

--- schemas.py
+++ schemas.py
@@ -1,3 +1,37 @@
-from pydantic import BaseModel
+import datetime
+from enum import Enum
+import datetime
 
-class
+import pydantic
+from pydantic import BaseModel, validator, EmailStr
+from pydantic.schema import date
+
+
+class User(BaseModel):
+    id: int
+    full_name: str
+    gender: str
+    birthday: date
+    residential_address: str
+
+
+# class Gender(str, Enum):
+#     male = 'Male'
+#     female = 'Female'
+
+
+class PhoneNumber(BaseModel):
+    id_user: int
+    type_of_number: str
+    phone_number: int
+
+class Email(BaseModel):
+    id_user: int
+    type_of_email: str
+    email: EmailStr
+
+
+data = {'id': '1', 'type_of_email': "work", 'email': "male@mail.ru"}
+
+user = Email(**data)
+print(user.dict)
