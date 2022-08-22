d982f0221ed4b42d8549b1a53f6b16acb8b28f48
anatolii <ostap_to@mail.ru>
2022-08-22 15:38:00 +0800

    did schemas

--- schemas.py
+++ schemas.py
@@ -1,37 +1,36 @@
-import datetime
 from enum import Enum
-import datetime
 
-import pydantic
 from pydantic import BaseModel, validator, EmailStr
 from pydantic.schema import date
+
+#  this class is used in User.gender for
+#  validation objects
+class GenderEnum(str, Enum):
+    male = 'Male'
+    female = 'Female'
+
+
+class TypePhoneNumberEnum(str, Enum):
+    city = 'city'
+    phone = 'phone'
 
 
 class User(BaseModel):
     id: int
     full_name: str
-    gender: str
+    gender: GenderEnum
     birthday: date
     residential_address: str
 
 
-# class Gender(str, Enum):
-#     male = 'Male'
-#     female = 'Female'
-
-
 class PhoneNumber(BaseModel):
     id_user: int
-    type_of_number: str
+    type_of_number: TypePhoneNumberEnum
     phone_number: int
+
 
 class Email(BaseModel):
     id_user: int
     type_of_email: str
     email: EmailStr
 
-
-data = {'id': '1', 'type_of_email': "work", 'email': "male@mail.ru"}
-
-user = Email(**data)
-print(user.dict)
