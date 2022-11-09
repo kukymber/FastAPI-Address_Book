798faee4fa593822888e8401662fe357e86d15df
anatolii <ostap_to@mail.ru>
2022-11-09 11:11:07 +0800

    did crud for email and phone number

--- views.py
+++ views.py
@@ -43,12 +43,11 @@ def read_user(id: int, session: Session = Depends(get_session)):
 def created_user(id: int, session: Session = Depends(get_session)):
     session.add(User)
     session.commit()
-    return f'created user with {id}'  # !!!
+    return f'created user with {id}'
 
 
 @app.put('/user/{id}', response_model=User)
 def update_user(id: int, session: Session = Depends(get_session)):
-
     # given id the user, which need update
     user_update = session.query(User).get(id)
     # If not found, raise exception and return 404 error
@@ -59,7 +58,6 @@ def update_user(id: int, session: Session = Depends(get_session)):
 
 @app.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(id: int, session: Session = Depends(get_session)):
-
     # given id the user, which need delete
     user_delete = session.query(User).get(id)
 
@@ -68,3 +66,84 @@ def delete_user(id: int, session: Session = Depends(get_session)):
         session.commit()
         raise HTTPException(status_code=404, detail=f'user with {id} not found')
     return user_delete
+
+
+"""User's phone number"""
+
+
+@app.get('/phone_number/{id}', response_model=PhoneNumber)
+def read_phone_number(id: int, session: Session = Depends(get_session)):
+    phone_num_get = session.query(models.PhoneNumber).get(id)
+
+    if not phone_num_get:
+        raise HTTPException(status_code=404, detail=f'phone_number with {id} not found')
+
+    return phone_num_get
+
+
+@app.post('/phone_number', response_model=PhoneNumber, status_code=status.HTTP_201_CREATED)
+def created_user(id: int, session: Session = Depends(get_session)):
+    session.add(PhoneNumber)
+    session.commit()
+
+    return f'created phone_number with {id}'
+
+
+@app.put('/phone_number/{id}', response_model=PhoneNumber)
+def update_user(id: int, session: Session = Depends(get_session)):
+    phone_num_update = session.query(PhoneNumber).get(id)
+
+    if not phone_num_update:
+        raise HTTPException(status_code=404, detail=f'phone_number with {id} not found')
+    return phone_num_update
+
+
+@app.delete('/phone_number/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int, session: Session = Depends(get_session)):
+    phone_num_delete = session.query(PhoneNumber).get(id)
+
+    if phone_num_delete:
+        session.delete(phone_num_delete)
+        session.commit()
+        raise HTTPException(status_code=404, detail=f'phone_number with {id} not found')
+    return phone_num_delete
+
+
+"""User's Email"""
+
+
+@app.get('/email/{id}', response_model=Email)
+def read_phone_number(id: int, session: Session = Depends(get_session)):
+    email_get = session.query(models.Email).get(id)
+
+    if not email_get:
+        raise HTTPException(status_code=404, detail=f'Email with {id} not found')
+
+    return email_get
+
+
+@app.post('/email', response_model=Email, status_code=status.HTTP_201_CREATED)
+def created_user(id: int, session: Session = Depends(get_session)):
+    session.add(Email)
+    session.commit()
+    return f'Created email with {id}'  # !!!
+
+
+@app.put('/email/{id}', response_model=PhoneNumber)
+def update_user(id: int, session: Session = Depends(get_session)):
+    email_update = session.query(Email).get(id)
+
+    if not email_update:
+        raise HTTPException(status_code=404, detail=f'Email with {id} not found')
+    return email_update
+
+
+@app.delete('/email/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int, session: Session = Depends(get_session)):
+    email_delete = session.query(Email).get(id)
+
+    if email_delete:
+        session.delete(email_delete)
+        session.commit()
+        raise HTTPException(status_code=404, detail=f'Email with {id} not found')
+    return email_delete
